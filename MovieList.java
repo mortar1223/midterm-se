1e4a8871e9331f6e8ff4b10daf6e96d1716bd865
Narutchai <narutchai.s@ku.th>
2019-03-16 15:19:11 +0700

    Add function change price

--- MovieList.java
+++ MovieList.java
@@ -3,7 +3,24 @@ import java.util.ArrayList;
 class MovieList {
 	ArrayList<Movie> movies = new ArrayList<Movie>();
 
-	public addMovie(Movie m){
+	public void addMovie(Movie m){
 		movies.add(m);
+	}
+
+	public String showAllMovies(){
+		String ouput = ""
+		for(int i = 0; i < movies.size(); i++){
+			output = movies.get(i).getName() + " " + movies.get(i).getDetail() + " " + movies.get(i).getPrice() + " " + movies.get(i).getShowTime() + " \n";
+		}
+		return output;
+	}
+
+	public void changeMoviePriceByName(String movie, double price){
+		for(int i = 0; i < movies.size(); i++){
+			if(movies.get(i).getName() == name){
+				movies.get(i).setPrice(price);
+				return;
+			}
+		}
 	}
 }
