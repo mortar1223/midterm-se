ad19b481917c7ace9ad9e932de41fdc37e035a0f
Narutchai <narutchai.s@ku.th>
2019-03-16 14:35:30 +0700

    Add function movie and movieList

--- Movie.java
+++ Movie.java
@@ -1,3 +1,45 @@
 class Movie {
-	
+	String name;
+	String detail;
+	double price;
+	String showTime;
+
+	public Movie(String name, String detail, double price, String showTime){
+		this.name = name;
+		this.detail = detail;
+		this.price = price;
+		this.showTime = showTime;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getDetail(){
+		return detail;
+	}
+
+	public void setDetail(String detail){
+		this.detail = detail
+	}
+
+	public double getPrice(){
+		return price;
+	}
+
+	public void setPrice(double price){
+		this.price = price;
+	}
+
+	public String getShowTime(){
+		return showTime;
+	}
+
+	public void setShowTime(String showTime){
+		this.showTime = showTime
+	}
 }
--- MovieList.java
+++ MovieList.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
 class MovieList {
-	
+	ArrayList<Movie> movies = new ArrayList<Movie>();
+
+	public addMovie(Movie m){
+		movies.add(m);
+	}
 }
